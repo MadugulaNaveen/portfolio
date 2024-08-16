920450d8ac6a33c017999b6ea4b280a9743bb985
Naveen Madugula <naveenmadugula7630@gmail.com>
2024-08-16 23:18:32 +0530

    changedPath

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import BottomPart from "./BottomPart";
 import { Parallax } from 'react-parallax';
-import image from "/Users/naveenm/Coding/Portfolio-master/src/Assets/back.jpg";
+import image from "../../Assets/back.jpg";
 import TopPart from "./TopPart";
 
 function Home() {
